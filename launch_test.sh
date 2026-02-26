bee75b510a5dcd7440648c52910919232bbf3407
Blake Lilly <lillyblake74@gmail.com>
2026-02-26 15:21:44 -0500

    venv

--- launch_test.sh
+++ launch_test.sh
@@ -5,7 +5,7 @@
 cd /home/blilly/Desktop/SolarDrone
 
 # Activate virtual environment
-source /home/blilly/Desktop/SolarDrone/venv/bin/activate
+source /home/blilly/venv/bin/activate
 
 # Run the MAVSDK script
 python3 AltitudeAndPanelDetection_MAVSDK.py >> /home/blilly/Desktop/SolarDrone/mission_debug.log 2>&1
